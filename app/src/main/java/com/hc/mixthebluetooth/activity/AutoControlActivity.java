7d2fa75d31cd57f0b58637a89d04668328135788
LAPTOP-D75Q22DA\ololee <ololeecn@gmail.com>
2022-02-02 19:31:40 +0800

    加入初始化代码

--- app/src/main/java/com/hc/mixthebluetooth/activity/AutoControlActivity.java
+++ app/src/main/java/com/hc/mixthebluetooth/activity/AutoControlActivity.java
@@ -1,15 +1,117 @@
 package com.hc.mixthebluetooth.activity;
 
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.hc.basiclibrary.viewBasic.BasActivity;
+import com.hc.basiclibrary.viewBasic.tool.IMessageInterface;
+import com.hc.bluetoothlibrary.DeviceModule;
 import com.hc.mixthebluetooth.R;
+import com.hc.mixthebluetooth.activity.single.HoldBluetooth;
+import com.hc.mixthebluetooth.databinding.ActivityAutoControlBinding;
+import com.hc.mixthebluetooth.fragment.AutoControllerFragment;
+import com.hc.mixthebluetooth.recyclerData.itemHolder.FragmentMessageItem;
+import java.util.List;
 
-public class AutoControlActivity extends AppCompatActivity {
+import static com.hc.mixthebluetooth.activity.CommunicationActivity.FRAGMENT_STATE_DATA;
+
+public class AutoControlActivity extends BasActivity {
+  private ActivityAutoControlBinding binding;
+  private FragmentManager fragmentManager;
+  private HoldBluetooth mHoldBluetooth;
+  private List<DeviceModule> modules;
+  private DeviceModule mErrorDisconnect;
+  private IMessageInterface mMessage;
+
+
+  private Handler mFragmentHandler = new Handler(new Handler.Callback() {
+    @Override
+    public boolean handleMessage(@NonNull Message msg) {
+      FragmentMessageItem item = (FragmentMessageItem) msg.obj;
+      mHoldBluetooth.sendData(item.getModule(),item.getByteData().clone());
+      return false;
+    }
+  });
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_auto_control);
+    binding = ActivityAutoControlBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    init();
+  }
 
+  @Override public void initAll() {
+    mHoldBluetooth = HoldBluetooth.getInstance();
+    initDataListener();
+  }
+
+  private void initDataListener() {
+    HoldBluetooth.OnReadDataListener dataListener = new HoldBluetooth.OnReadDataListener() {
+      @Override public void readData(String mac, byte[] data) {
+        mMessage.readData(FRAGMENT_STATE_DATA,modules.get(0),data);
+      }
+
+      @Override public void reading(boolean isStart) {
+
+      }
+
+      @Override public void connectSucceed() {
+        modules = mHoldBluetooth.getConnectedArray();
+      }
+
+      @Override public void errorDisconnect(DeviceModule deviceModule) {
+        if (mErrorDisconnect == null) {//判断是否已经重复连接
+          mErrorDisconnect = deviceModule;
+          if (mHoldBluetooth != null && deviceModule != null) {
+            mFragmentHandler.postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                mHoldBluetooth.connect(deviceModule);
+              //  setState(CONNECTING);//设置正在连接状态
+              }
+            },2000);
+            return;
+          }
+        }
+       // setState(DISCONNECT);//设置断开状态
+        if (deviceModule != null)
+          toast("连接" + deviceModule.getName() + "失败，点击右上角的已断线可尝试重连", Toast.LENGTH_LONG);
+        else
+          toast("连接模块失败，请返回上一个页面重连", Toast.LENGTH_LONG);
+      }
+
+      @Override public void readNumber(int number) {
+
+      }
+
+      @Override public void readLog(String className, String data, String lv) {
+
+      }
+    };
+
+    mHoldBluetooth.setOnReadListener(dataListener);
+  }
+
+  private void init() {
+    mMessage = AutoControllerFragment.newInstance("","");
+    fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.auto_controller_frame,
+        (Fragment) mMessage);
+    fragmentTransaction.commitAllowingStateLoss();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (modules != null)
+      mHoldBluetooth.disconnect(modules.get(0));
   }
 }
--- app/src/main/java/com/hc/mixthebluetooth/fragment/AutoControllerFragment.java
+++ app/src/main/java/com/hc/mixthebluetooth/fragment/AutoControllerFragment.java
@@ -1,18 +1,35 @@
 package com.hc.mixthebluetooth.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.hc.basiclibrary.titleBasic.DefaultNavigationBar;
+import com.hc.basiclibrary.viewBasic.BasFragment;
+import com.hc.bluetoothlibrary.DeviceModule;
 import com.hc.mixthebluetooth.R;
+import com.hc.mixthebluetooth.activity.CommunicationActivity;
+import com.hc.mixthebluetooth.activity.tool.Analysis;
+import com.hc.mixthebluetooth.databinding.FragmentAutoControllerBinding;
+import com.hc.mixthebluetooth.recyclerData.itemHolder.FragmentMessageItem;
+import com.hc.mixthebluetooth.storage.Storage;
+import java.util.Arrays;
 
-public class AutoControllerFragment extends Fragment {
+public class AutoControllerFragment extends BasFragment {
   private static final String ARG_PARAM1 = "param1";
   private static final String ARG_PARAM2 = "param2";
 
+  private Runnable mRunnable;//循环发送的线程
+  private Handler mHandler;
+
+  private DeviceModule module;
+
   private String mParam1;
   private String mParam2;
+  private FragmentAutoControllerBinding binding;
 
   public AutoControllerFragment() {
   }
@@ -26,7 +43,7 @@ public class AutoControllerFragment extends Fragment {
     return fragment;
   }
 
-  @Override
+ /* @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     if (getArguments() != null) {
@@ -39,5 +56,57 @@ public class AutoControllerFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_auto_controller, container, false);
+  }*/
+
+  @Override public int setFragmentViewId() {
+    return R.layout.fragment_auto_controller;
+  }
+
+  @Override public void initAll() {
+    binding= FragmentAutoControllerBinding.inflate(getLayoutInflater());
+  }
+
+  @Override public void initAll(View view, Context context) {
+    super.initAll(view, context);
+  }
+
+  @Override public void setHandler(Handler handler) {
+    this.mHandler = handler;
+  }
+
+  @Override public void readData(int state, Object o, byte[] data) {
+    switch (state) {
+      case CommunicationActivity.FRAGMENT_STATE_DATA:
+        if (module == null) {
+          module = (DeviceModule) o;
+        }
+        if (data != null) {
+          String strData = Analysis.getByteToString(data, true);
+          log("ololeeDetail: "+strData);
+          String[] dataArray = strData.split("[a-zA-Z]");
+          log("ololeeDetail: "+ Arrays.toString(dataArray));
+          binding.lateralDeviationTv.setText(dataArray[1]);
+          binding.courseDeviationTv.setText(dataArray[2]);
+          binding.frontWheelAngleTv.setText(dataArray[3]);
+          binding.vehicleDirectionTv.setText(dataArray[4]);
+          //D0.236B-0.125C56.245H5.546
+         /*
+          mDataList.add(new FragmentMessageItem(Analysis.getByteToString(data,isReadHex), isShowTime?Analysis.getTime():null, false, module,isShowMyData));
+          mAdapter.notifyDataSetChanged();
+          mRecyclerView.smoothScrollToPosition(mDataList.size());
+          mReadNumberTV.setText(String.valueOf(Integer.parseInt(mReadNumberTV.getText().toString())+Analysis.lengthArray(data)));
+          */
+
+        }
+        break;
+      case CommunicationActivity.FRAGMENT_STATE_NUMBER:
+       /* mSendNumberTv.setText(
+            String.valueOf(Integer.parseInt(mSendNumberTv.getText().toString()) + ((int) o)));
+        setUnsentNumberTv();*/
+        break;
+      case CommunicationActivity.FRAGMENT_STATE_SEND_SEND_TITLE:
+        //mTitle = (DefaultNavigationBar) o;
+        break;
+    }
   }
 }
