76a448dd054e4a47cf3132f66421c4474cdba742
LAPTOP-D75Q22DA\ololee <ololeecn@gmail.com>
2022-02-04 11:58:07 +0800

    发送代码测试

--- app/src/main/java/com/hc/mixthebluetooth/data/utils/DataCastUtils.java
+++ app/src/main/java/com/hc/mixthebluetooth/data/utils/DataCastUtils.java
@@ -3,11 +3,21 @@ package com.hc.mixthebluetooth.data.utils;
 public class DataCastUtils {
 
   public static int byteArray2int(byte[] data, int offset) {
+    for (int i = 0; i < 4; i++) {
+      data[offset + i] = (byte) (data[offset + i] - 128);
+    }
+    return ((((int) data[offset + 3]) + 128) << 24)
+        | ((((int) data[offset + 2]) + 128) << 16)
+        | ((((int) data[offset + 1]) + 128) << 8)
+        | (((int) data[offset]) + 128);
+  }
+
+  /*public static int byteArray2int(byte[] data, int offset) {
     return ((((int) data[offset + 3] - 128) + 128) << 24)
         | ((((int) data[offset + 2] - 128) + 128) << 16)
         | ((((int) data[offset + 1] - 128) + 128) << 8)
         | (((int) data[offset] - 128) + 128);
-  }
+  }*/
 
   public static float byte2float(byte[] data, int offset) {
     return Float.intBitsToFloat(byteArray2int(data, offset));
